1df37cc6c3265ef68b4300cfb2346a9e43f9b705
Mahi956 <itsmahi.sharma17@gmail.com>
2026-05-16 11:05:05 +0530

    16th may

--- Qforloo.py
+++ Qforloo.py
@@ -3,12 +3,25 @@
 #     print(num)
 #     print(number)
 
-name = ["MAHI"]
-for i in name:
-    print (i)
-    if (i== "I"):
-        print('you are special ')
+# name = ["MAHI"]
+# for i in name:
+#     print (i)
+#     if (i== "I"):
+#         print('you are special ')
 
 # colours =["RED, GREEN , BLUE "]
 # for i in colours:
 #     print(i)
+    
+    
+for i  in range(5):
+    print(i) 
+ 
+print("____________")    
+
+for k in range(1,6):
+    print(k)
+print("____________")
+    
+for l in range(0,51,2):
+    print(l)
